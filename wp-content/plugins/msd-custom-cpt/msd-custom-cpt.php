dae301d329f8c3170cc0a183da3f145be8e4e49e
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-02-10 15:17:24 -0500

    add news, team

--- wp-content/plugins/msd-custom-cpt/msd-custom-cpt.php
+++ wp-content/plugins/msd-custom-cpt/msd-custom-cpt.php
@@ -101,7 +101,12 @@ if (!class_exists('MSDCustomCPT')) {
         	register_activation_hook(__FILE__, array(&$this,'check_requirements'));
         	//get sub-packages
         	requireDir(plugin_dir_path(__FILE__).'/lib/inc');
-            if(class_exists('MSDLocationCPT')){
+            if(class_exists('MSDNewsCPT')){
+                $this->news_class = new MSDNewsCPT();
+                register_activation_hook( __FILE__, create_function('','flush_rewrite_rules();') );
+                register_deactivation_hook( __FILE__, create_function('','flush_rewrite_rules();') );
+            }
+            /*if(class_exists('MSDLocationCPT')){
                 $this->location_class = new MSDLocationCPT();
                 register_activation_hook( __FILE__, create_function('','flush_rewrite_rules();') );
                 register_deactivation_hook( __FILE__, create_function('','flush_rewrite_rules();') );
@@ -110,7 +115,7 @@ if (!class_exists('MSDCustomCPT')) {
                 $this->project_class = new MSDProjectCPT();
                 register_activation_hook( __FILE__, create_function('','flush_rewrite_rules();') );
                 register_deactivation_hook( __FILE__, create_function('','flush_rewrite_rules();') );
-            }
+            }*/
             if(class_exists('MSDTeamCPT')){
                 $this->cpt_class = new MSDTeamCPT();
                 register_activation_hook( __FILE__, create_function('','flush_rewrite_rules();') );
