d6c40e2ea3411a3152c59c899b2497a153f38590
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-03-03 00:11:48 -0500

    tweak excerpt ending

--- wp-content/themes/veritas/lib/inc/genesis_tweak_functions.php
+++ wp-content/themes/veritas/lib/inc/genesis_tweak_functions.php
@@ -183,6 +183,11 @@ function msdlab_author_image(){
         
         print $thumb;
 }
+
+function new_excerpt_more( $more ) {
+    return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">Read More</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
 /**
  * Custom blog loop
  */
