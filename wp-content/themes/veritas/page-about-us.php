1ccf96a8ecea4310b7dfd4caf70e072889320330
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-03-02 22:17:50 -0500

    about us sidebar

--- wp-content/themes/veritas/page-about-us.php
+++ wp-content/themes/veritas/page-about-us.php
@@ -2,5 +2,14 @@
 /*
 Template Name: About Us Section Template
 */
+remove_action('genesis_sidebar','genesis_do_sidebar');
+add_action('genesis_sidebar','msdlab_do_about_us_sidebar');
+function msdlab_do_about_us_sidebar(){
+    if(is_active_sidebar('aboutus')){
+        print '<div class="about-us-widget-area row">';
+        dynamic_sidebar('aboutus');
+        print '</div>';
+    }
+}
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_content_sidebar' );
 genesis();
