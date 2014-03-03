99705096254f3cc5b6f66c835ffb739966166d2e
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-03-03 00:13:26 -0500

    add a carat

--- wp-content/themes/veritas/lib/inc/genesis_tweak_functions.php
+++ wp-content/themes/veritas/lib/inc/genesis_tweak_functions.php
@@ -185,7 +185,7 @@ function msdlab_author_image(){
 }
 
 function new_excerpt_more( $more ) {
-    return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">Read More</a>';
+    return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">Read More ></a>';
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
 /**
