63c199983e3b6ce3b62f8543b672ed8da7918eb6
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-03-06 13:18:26 -0500

    add external to blank window thingy

--- wp-content/themes/veritas/lib/js/theme-jquery.js
+++ wp-content/themes/veritas/lib/js/theme-jquery.js
@@ -47,6 +47,24 @@ jQuery(document).ready(function($) {
         }
     });
     
+    // add target="_blank" to all *external* 
+    var internal_urls = Array('truepoint.oc','72.52.131.35','truepointwealth.com','truepointinc.com');
+    $('a').attr('target',function(){
+        var url = $(this).attr('href');
+        if(url == '#'){
+            return '_self';
+        } else {
+            var i=0;
+            while (internal_urls[i]){
+                if(strripos(url, internal_urls[i], 0)){
+                    return '_self';
+                }
+                i++;
+            }
+            return '_blank';
+        }
+    });
+    
 	/*RESPONSIVE NAVIGATION, COMBINES MENUS EXCEPT FOR FOOTER MENU*/
 
     //jQuery('.menu').not('#footer .menu, #footer-widgets .menu').wrap('<div id="nav-response" class="nav-responsive">');
@@ -87,3 +105,33 @@ jQuery(document).ready(function($) {
                 } );
             }
 });
+
+function strripos(haystack, needle, offset) {
+  //  discuss at: http://phpjs.org/functions/strripos/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // bugfixed by: Onno Marsman
+  // bugfixed by: Brett Zamir (http://brett-zamir.me)
+  //    input by: saulius
+  //   example 1: strripos('Kevin van Zonneveld', 'E');
+  //   returns 1: 16
+
+  haystack = (haystack + '')
+    .toLowerCase();
+  needle = (needle + '')
+    .toLowerCase();
+
+  var i = -1;
+  if (offset) {
+    i = (haystack + '')
+      .slice(offset)
+      .lastIndexOf(needle); // strrpos' offset indicates starting point of range till end,
+    // while lastIndexOf's optional 2nd argument indicates ending point of range from the beginning
+    if (i !== -1) {
+      i += offset;
+    }
+  } else {
+    i = (haystack + '')
+      .lastIndexOf(needle);
+  }
+  return i >= 0 ? i : false;
+}
