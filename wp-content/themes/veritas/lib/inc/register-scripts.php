f9c352d16a9736b4117a1c3c39ccd196b966e4db
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-03-09 17:39:12 -0400

    fix style vs script note

--- wp-content/themes/veritas/lib/inc/register-scripts.php
+++ wp-content/themes/veritas/lib/inc/register-scripts.php
@@ -23,7 +23,7 @@ function msdlab_add_styles() {
             $queue[] = 'msd-homepage-style';
         }    
         if($is_IE){
-            wp_enqueue_script('ie-style',get_stylesheet_directory_uri().'/lib/css/ie.css');
+            wp_enqueue_style('ie-style',get_stylesheet_directory_uri().'/lib/css/ie.css',$queue);
             $queue[] = 'ie-style';
         }    
     }
