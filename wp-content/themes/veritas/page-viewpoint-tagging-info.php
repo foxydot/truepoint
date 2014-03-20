1ae055c204f72256e0463b0e88d91302291ca722
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-03-20 15:50:56 -0400

    maybe posts conflicting?

--- wp-content/themes/veritas/page-viewpoint-tagging-info.php
+++ wp-content/themes/veritas/page-viewpoint-tagging-info.php
@@ -1,17 +1,18 @@
 <?php
 function get_all_content_info_into_table(){
+    print "hello";
     $args = array(
         'post_type' => 'post',
         'posts_per_page' => -1
     );
-    $posts = new WP_Query($args);
-    if ( $posts->have_posts() ) {
+    $viewpoints = new WP_Query($args);
+    if ( $viewpoints->have_posts() ) {
             echo '
             <style>td{border: 1px solid #999;}</style>
             <table>
             <tr><th>Title</th><th>Author</th><th>Categories</th><th>Tags</th></tr>';
-        while ( $posts->have_posts() ) {
-            $posts->the_post();
+        while ( $viewpoints->have_posts() ) {
+            $viewpoints->the_post();
             echo '<tr>';
             echo '<td>' . get_the_title() . '</td>';
             echo '<td>' . get_the_author() . '</td>';
