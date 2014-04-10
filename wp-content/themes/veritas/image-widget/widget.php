4579a6d59c8493dce39056ae1bf21cb9f5493f9c
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-04-10 13:47:08 -0400

    move text above content on homepage widget

--- wp-content/themes/veritas/image-widget/widget.php
+++ wp-content/themes/veritas/image-widget/widget.php
@@ -9,20 +9,38 @@ if ( !defined('ABSPATH') )
 	die('-1');
 
 echo $before_widget;
-
-if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }
-echo '<div class="widget-content">';
-echo $this->get_image_html( $instance, true );
-
-if ( !empty( $description ) ) {
-	echo '<div class="'.$this->widget_options['classname'].'-description" >';
-	echo wpautop( $description );
-	echo "</div>";
+if(is_front_page()){
+    if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }
+    echo '<div class="widget-content">';
+    
+    if ( !empty( $description ) ) {
+        echo '<div class="'.$this->widget_options['classname'].'-description" >';
+        echo wpautop( $description );
+        echo "</div>";
+    }
+        echo $this->get_image_html( $instance, true );
+    
+    if ( $link ) {
+        $linktext = $linktext != ''?$linktext:'Read More >';
+        echo '<div class="link"><a class="'.$this->widget_options['classname'].'-link readmore" href="'.$link.'" target="'.$linktarget.'">'.$linktext.' ></a><div class="clear"></div></div>';
+    }
+    echo '<div class="clear"></div>
+    </div>';
+}else{
+    if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }
+    echo '<div class="widget-content">';
+    echo $this->get_image_html( $instance, true );
+    
+    if ( !empty( $description ) ) {
+    	echo '<div class="'.$this->widget_options['classname'].'-description" >';
+    	echo wpautop( $description );
+    	echo "</div>";
+    }
+    if ( $link ) {
+    	$linktext = $linktext != ''?$linktext:'Read More >';
+    	echo '<div class="link"><a class="'.$this->widget_options['classname'].'-link readmore" href="'.$link.'" target="'.$linktarget.'">'.$linktext.' ></a><div class="clear"></div></div>';
+    }
+    echo '<div class="clear"></div>
+    </div>';
 }
-if ( $link ) {
-	$linktext = $linktext != ''?$linktext:'Read More >';
-	echo '<div class="link"><a class="'.$this->widget_options['classname'].'-link readmore" href="'.$link.'" target="'.$linktarget.'">'.$linktext.' ></a><div class="clear"></div></div>';
-}
-echo '<div class="clear"></div>
-</div>';
 echo $after_widget;
