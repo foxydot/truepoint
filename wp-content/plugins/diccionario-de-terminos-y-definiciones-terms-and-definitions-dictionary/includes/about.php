cde38b79ce27bab13427315d1f12ea7641ab1803
msdlab <catherine@msdlab.com>
2014-09-23 13:06:59 -0400

    maybe add some plugins

---
+++ wp-content/plugins/diccionario-de-terminos-y-definiciones-terms-and-definitions-dictionary/includes/about.php
@@ -0,0 +1,295 @@
+<div class="wrap">
+	<h2>Acerca de</h2>
+	
+	<div class='welcome-panel'>
+		<h3>
+			<span>
+				Diccionario de Términos y Definiciones
+			</span>
+		</h3>
+		<div class='inside'>
+			<p><strong>Nombre</strong>: Diccionario de Términos y Definiciones</br>
+			<strong>Aplicación</strong>: Plugin de WordPress</br>
+			<strong>Creado por</strong>: Codigonexo - <a href='http://www.codigonexo.com/' target='_blank'>http://www.codigonexo.com/</a></br>
+			<strong>Autor</strong>: Juan Viñas Traverso para <a href='http://www.codigonexo.com/' target='_blank'>Codigonexo</a></br>
+			<strong>Versión Actual</strong>: 1.5 - 20 de Diciembre de 2012</br>
+			<strong>Descripción</strong>: Crea un diccionario de términos y definiciones interactivo mediante etiquetas.</p>
+		</div>
+	</div>
+		
+	<div class='welcome-panel'>
+		<h3>
+			<span>
+				Licencia GPLv2
+			</span>
+		</h3>
+		<div class='inside'>
+			<div id="licencia">
+			    <p>Copyright (C) 2012, Codigonexo</p>
+		
+			    <p>This program is free software: you can redistribute it and/or modify
+			    it under the terms of the GNU General Public License as published by
+			    the Free Software Foundation, either version 2 of the License, or
+			    (at your option) any later version.</p>
+			
+			    <p>This program is distributed in the hope that it will be useful,
+			    but WITHOUT ANY WARRANTY; without even the implied warranty of
+			    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+			    GNU General Public License for more details.</p>
+			
+			    <p>You should have received a copy of the GNU General Public License
+			    along with this program.  If not, see <http://www.gnu.org/licenses/>.</p>
+			
+			    <p>---</p>
+				
+			    <p>(Esta traducción es meramente informativa y carece de validez legal:)</p>
+				
+			    <p>Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo 
+			    los términos de la Licencia Pública General de GNU según es publicada por la 
+			    Fundación para el Software Libre, bien de la versión 2 de dicha Licencia o 
+			    bien (según su elección) de cualquier versión posterior.</p>
+			
+			    <p>Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA 
+			    GARANTÍA, incluso sin la garantía MERCANTIL implícita o sin garantizar la 
+			    CONVENIENCIA PARA UN PROPÓSITO PARTICULAR. Véase la Licencia Pública General de 
+			    GNU para más detalles.</p>
+			
+			    <p>Debería haber recibido una copia de la Licencia Pública General junto con este 
+			    programa. Si no ha sido así, visite <http://www.gnu.org/licenses/>.</p>
+			</div>			
+		</div>
+	</div>
+
+	<h3>Historial de Cambios</h3>
+	<table class="wp-list-table widefat changelog">
+		<thead>
+			<tr>
+				<th class='manage-column column-version'>
+					Versión
+				</th>
+				<th class='manage-column column-date'>
+					Fecha
+				</th>
+				<th class='manage-column column-comments'>
+					Comentarios
+				</th>
+			</tr>
+		</thead>
+		<tbody id="the-list">
+			<!-- 1.5.1 -->
+			<tr>
+				<td>
+					<strong>1.5.1</strong>
+				</td>
+				<td>
+					24/12/2012
+				</td>
+				<td>
+					El plugin ahora utiliza la versión de jQuery integrada en WordPress, y carga jQuery Tools desde la carpeta /js.
+				</td>
+			</tr>
+			<!-- 1.5 -->
+			<tr>
+				<td>
+					<strong>1.5</strong>
+				</td>
+				<td>
+					20/12/2012
+				</td>
+				<td>
+					Añadida página "Acerca de".</br>
+
+					Redistribuídas las páginas. Ahora "Ajustes" se llama "Opciones" y lo podemos encontrar en el submenú "Definiciones".</br>
+
+					Arreglados problemas menores.</br>
+
+					Al buscar las definiciones por título, no es sensible a mayúsculas y minúsculas.</br>
+
+				</td>
+			</tr>
+			<!-- 1.4.3 -->
+			<tr>
+				<td>
+					1.4.3
+				</td>
+				<td>
+					20/12/2012
+				</td>
+				<td>
+					Eliminado código inservible.
+				</td>
+			</tr>			
+			<!-- 1.4.2 -->
+			<tr>
+				<td>
+					1.4.2
+				</td>
+				<td>
+					20/12/2012
+				</td>
+				<td>
+					Widget de Familias mejorado con anidación de Familias. Deshabilitada la opción "Profundidad".</br>
+
+					Mejorada la autodetección en las entradas simples de Definiciones. Ahora la definicion no aparecerá definida en su propia definición.
+
+				</td>
+			</tr>			
+			<!-- 1.4.1 -->
+			<tr>
+				<td>
+					1.4.1
+				</td>
+				<td>
+					19/12/2012
+				</td>
+				<td>
+					Puedes establecer la auto detección en aquellos posts en los que no exista ninguna etiqueta [def]. Cuando exista, sólo aparecerán las definiciones que se establezcan a mano.		
+				</td>
+			</tr>			
+			<!-- 1.4 -->
+			<tr>
+				<td>
+					<strong>1.4</strong>
+				</td>
+				<td>
+					19/12/2012
+				</td>
+				<td>
+					Nueva funcionalidad: auto detección de términos. Todas las coincidencias serán mostradas como términos si se selecciona esta opción.</br>
+
+					Mejorado estilo de los términos que aparecen el texto, heredando los del texto en el que están.
+				</td>
+			</tr>			
+			<!-- 1.3.6 -->
+			<tr>
+				<td>
+					1.3.6
+				</td>
+				<td>
+					19/12/2012
+				</td>
+				<td>
+					Mejorado añadiendo al tooltip z-index: 99.</br>
+					Si no se establece el atributo URL, el término no aparece como un enlace.
+				</td>
+			</tr>
+			<!-- 1.3.5 -->
+			<tr>
+				<td>
+					1.3.5
+				</td>
+				<td>
+					18/12/2012
+				</td>
+				<td>
+					Adaptado a WordPress 3.5.</br>
+					Opciones de selección de color del plugin adaptadas a WordPress 3.5.
+				</td>
+			</tr>
+			<!-- 1.3.4 -->
+			<tr>
+				<td>
+					1.3.4
+				</td>
+				<td>
+					18/12/2012
+				</td>
+				<td>
+					Añadidas opciones del widget: Título y Profundidad de la lista.</br>
+					Mejoradas las opciones del plugin.
+				</td>
+			</tr>			
+			<!-- 1.3.3 -->
+			<tr>
+				<td>
+					1.3.3
+				</td>
+				<td>
+					18/12/2012
+				</td>
+				<td>
+					Arreglado Widget
+				</td>
+			</tr>
+			<!-- 1.3.2 -->
+			<tr>
+				<td>
+					1.3.2
+				</td>
+				<td>
+					27/11/2012
+				</td>
+				<td>
+					Añadido Widget
+				</td>
+			</tr>
+			<!-- 1.3.1 -->
+			<tr>
+				<td>
+					1.3.1
+				</td>
+				<td>
+					27/11/2012
+				</td>
+				<td>
+					Añadidos los iconos de la aplicación, hechos por Layla Sierra.
+				</td>
+			</tr>
+			<!-- 1.3 -->
+			<tr>
+				<td>
+					<strong>1.3</strong>
+				</td>
+				<td>
+					26/11/2012
+				</td>
+				<td>
+					Añadida opción de URL al short-tag: [def url="http://url.com"]Término[/def].
+				</td>
+			</tr>
+			<!-- 1.2 -->
+			<tr>
+				<td>
+					<strong>1.2</strong>
+				</td>
+				<td>
+					26/11/2012
+				</td>
+				<td>
+					Añadida funcionalidad a la página de opciones</br>
+					Integración de las opciones con el resultado generado</br>
+					Añadidas opciones de color de texto del tooltip y opacidad.</br>
+					Añadido soporte para campos HTML 5 en las opciones.</br>
+					Añadido soporte para reconocer Slug e ID de las definiciones.</br>
+				</td>
+			</tr>
+			<!-- 1.1 -->
+			<tr>
+				<td>
+					<strong>1.1</strong>
+				</td>
+				<td>
+					24/11/2012
+				</td>
+				<td>
+					Añadida página de opciones
+				</td>
+			</tr>
+			<!-- 1.0 -->
+			<tr>
+				<td>
+					<strong>1.0</strong>
+				</td>
+				<td>
+					23/11/2012
+				</td>
+				<td>
+					Plugin con funcionalidad básica creado. 
+					Plugin mejorado con funcionalidad jQuery.
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	</br>
+	<i>Desarrollado por <a href='http://www.codigonexo.com/'>Codigonexo</a></i>
+</div>
