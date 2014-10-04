20deff7ce3083150fb3dfdac608c11345a8390de
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-10-03 22:07:07 -0400

    correct bootstrap.

--- wp-content/themes/veritas/lib/inc/genesis_bootstrap_hooks.php
+++ wp-content/themes/veritas/lib/inc/genesis_bootstrap_hooks.php
@@ -6,7 +6,7 @@ add_filter( 'genesis_attr_breadcrumb', 'msdlab_bootstrap_breadcrumb', 10);
 add_filter( 'genesis_attr_content-sidebar-wrap', 'msdlab_bootstrap_content_sidebar_wrap', 10);
 add_filter( 'genesis_attr_content', 'msdlab_bootstrap_content', 10);
 add_filter( 'genesis_attr_sidebar-primary', 'msdlab_bootstrap_sidebar', 10);
-add_filter( 'genesis_attr_sidebar-alt', 'msdlab_bootstrap_sidebar_2', 10);
+add_filter( 'genesis_attr_sidebar-secondary', 'msdlab_bootstrap_sidebar_alt', 10);
 
  /*** Bootstrappin **/
 
@@ -103,7 +103,7 @@ function msdlab_bootstrap_sidebar( $attributes ){
     return $attributes;
 }
 
-function msdlab_bootstrap_sidebar2( $attributes ){
+function msdlab_bootstrap_sidebar_alt( $attributes ){
     $layout = genesis_site_layout();
     $template = get_page_template();
     switch($layout){
