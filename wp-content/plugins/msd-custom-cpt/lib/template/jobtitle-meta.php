8b46ffdf89ca8e6bbcfb0a26aab203f749038a02
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-10-20 16:32:40 -0400

    cleanup the git index

--- wp-content/plugins/msd-custom-cpt/lib/template/jobtitle-meta.php
+++
@@ -1,6 +0,0 @@
-<div class="my_meta_control" id="jobtitle_metabox">
-	<p>
-		<?php $mb->the_field('jobtitle'); ?>
-		<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>" />
-	</p>
-</div>
