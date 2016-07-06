b5c0db3efc9336a5d40a7392ffae5c4615950458
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-07-05 23:36:23 -0400

    single team styling of sidebar

--- assets/themes/master-of-coin/single-team_member.php
+++ assets/themes/master-of-coin/single-team_member.php
@@ -9,7 +9,7 @@ function msdlab_do_team_sidebar(){
 }
 add_action('genesis_entry_content',array(&$msd_team_display,'msd_add_team_member_headshot'),2);
 add_action('msdlab_after_team_member_headshot',array(&$msd_team_display,'msd_team_member_contact_info'));
-add_action('genesis_entry_header',array(&$msd_team_display,'msd_do_team_member_job_title'));
+add_action('msdlab_title_area',array(&$msd_team_display,'msd_do_team_member_job_title'));
 remove_action( 'genesis_entry_header', 'genesis_post_info', 12);
 //global $wp_filter; ts_var( $wp_filter['genesis_entry_header'] );
 genesis();
