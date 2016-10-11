66fe7e01bb94b89d798e31f91929fa379af11faf
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-10-11 11:17:13 -0400

    add modal to footer universally

--- assets/themes/master-of-coin/lib/inc/genesis_tweak_functions.php
+++ assets/themes/master-of-coin/lib/inc/genesis_tweak_functions.php
@@ -535,7 +535,25 @@ function msdlab_newer_link_text() {
 }
 
 /*** FOOTER ***/
-
+add_action('wp_footer','msdlab_add_footer_subscribe_modal');
+function msdlab_add_footer_subscribe_modal($atts){
+    print '<div class="modal fade" id="subscribe" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Subscribe to the Truepoint Bulletin</h4>
+      </div>
+      <div class="modal-body">
+      do_shortcode('[gravityform id="2" name="Truepoint Subscription" title="false" ajax="true"]);
+    print '
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>';
+}
 
 /*** SITEMAP ***/
 /**
