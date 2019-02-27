315680aeaede785d7e3eba178a0e8f83a8297127
CMO <catherine@msdlab.com>
2019-02-27 13:36:07 -0500

    switch http:// to https://

--- assets/plugins/msd-custom-cpt/lib/template/newsurl-meta.php
+++ assets/plugins/msd-custom-cpt/lib/template/newsurl-meta.php
@@ -1,6 +1,6 @@
 <div class="my_meta_control" id="newsurl_metabox">
 	<p>
 		<?php $mb->the_field('newsurl'); ?>
-		<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>" placeholder="http://" />
+		<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>" placeholder="https://" />
 	</p>
 </div>
